269f3fac9ccac1c08acbca3cb657e24d167cf7e4
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-02-17 21:12:25 +0100

    add the project tree

---
+++ pages/connection.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/connection.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <section class="connection-panel" id="connection">
+
+    </section>
+</body>
+</html>
